87de63b765cfe2bb25877a23f61a916406a4859e
Francisco Garcia <ciscog11@msn.com>
2016-04-26 10:42:58 -0600

    added to css

--- public_html/me/index.php
+++ public_html/me/index.php
@@ -5,21 +5,24 @@
 		<title>Francisco</title>
 		<link href="../css/stylesheet.css" rel="stylesheet" type="text/css"/>
 		<link href='https://fonts.googleapis.com/css?family=Covered+By+Your+Grace' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Alegreya+Sans:400,800' rel='stylesheet' type='text/css'>
+		<link href='https://fonts.googleapis.com/css?family=Source+Sans+Pro:400,300,600' rel='stylesheet' type='text/css'>
 	</head>
 	<body>
-		<h1>About Me</h1>
+		<h1 id="about-me"> About Me</h1>
 		<hr>
 			<h2>Background</h2>
 				<p>
 					I'm from Seattle but since High School I've split my time between here in Albuquerque and South Carolina. Other than being a student at UNM I've been working as a Tennis Pro at a couple different Acadamies.
 				</p>
-		<h2>Hobbies</h2>
+		<h2 id="hobbies"> Hobbies</h2>
 			<p>
 				I enjoy playing tennis for fun outside of work. I also enjoy PC gaming when I have free time.
 			</p>
-		<h2>Office with a View</h2>
-			<img src="http://i.imgur.com/TNI4MoR.jpg" alt="newView"
-			width="1280px"
-			height="720px"/>
+		<h2 id="office"> Office with a View</h2>
+			<img src="http://i.imgur.com/TNI4MoR.jpg" class="view"
+			height="480px"
+			width="854px"/>
+		<hr>
 	</body>
 </html>
